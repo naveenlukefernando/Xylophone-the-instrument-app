23d2d5b06b8e94d93b123eae7615c7892a9c0e72
naveenlukefernando <naveenlukefernandog3@gmail.com>
2018-06-28 14:25:39 +0530

    #1 UI change

---
+++ app/src/main/java/com/luke/xylophone/Activity_home.java
@@ -0,0 +1,13 @@
+package com.luke.xylophone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
