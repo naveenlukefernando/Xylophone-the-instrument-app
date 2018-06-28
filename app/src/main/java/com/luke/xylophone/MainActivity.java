05ae5f728b16953d938a2c3d08d52c5fa58ce258
naveenlukefernando <naveenlukefernandog3@gmail.com>
2018-06-28 23:18:36 +0530

    #5 few UI changes

--- app/src/main/java/com/luke/xylophone/MainActivity.java
+++ app/src/main/java/com/luke/xylophone/MainActivity.java
@@ -1,13 +1,29 @@
 package com.luke.xylophone;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME=1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, Activity_home.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME);
+
+
     }
 }
