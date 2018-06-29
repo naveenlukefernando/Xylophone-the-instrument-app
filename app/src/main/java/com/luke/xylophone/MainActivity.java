93a5b9501c62ee8b90b02a85ae75f6d235df00ce
naveenlukefernando <naveenlukefernandog3@gmail.com>
2018-06-29 13:33:19 +0530

    #5 UI RESPONSE

--- app/src/main/java/com/luke/xylophone/MainActivity.java
+++ app/src/main/java/com/luke/xylophone/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME=1500;
+    private static int SPLASH_TIME=4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
